6a08b872c7642658e34027984d2dd7d887bf3a28
gaborBKI <gabor.bki@gmail.com>
2019-06-24 10:38:05 +0200

    Changed driver type to env. variable.

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -3,14 +3,28 @@ import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginTest {
 
-    private static WebDriver driver = new FirefoxDriver();
-    private Util util = new Util(driver);
+    private static WebDriver driver;
+    private static Util util;
+
+    @BeforeAll
+    public static void setUp(){
+        switch (System.getenv("driverType")){
+            case "Chrome":
+                driver = new ChromeDriver();
+                break;
+            case "Firefox":
+                driver = new FirefoxDriver();
+                break;
+        }
+        util = new Util(driver);
+    }
 
     @Test
     public void happyPathTest(){
