05b39ea7bca351b6f26a921656d62e190216f313
gaborBKI <gabor.bki@gmail.com>
2019-06-24 14:49:22 +0200

    Validation of correct all projects site appearing done.

--- src/test/java/BrowseProjectsTest.java
+++ src/test/java/BrowseProjectsTest.java
@@ -32,14 +32,24 @@ public class BrowseProjectsTest {
     @Test
     public void projectListAppears() {
         WebDriverWait wait = new WebDriverWait(driver, 10);
-        WebElement element = wait.until(
-                ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"header\"]/nav")));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"header\"]/nav")));
         WebElement projectsDropdown = driver.findElement(By.id("browse_link"));
         projectsDropdown.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("project_view_all_link_lnk")));
         WebElement viewAllButton = driver.findElement(By.id("project_view_all_link_lnk"));
         viewAllButton.click();
         WebElement projectList = driver.findElement(By.id("browse-projects-page"));
         Assert.assertNotNull(projectList);
+    }
+
+    @Test
+    public void projectPageValid(){
+        String expectedURL = "https://jira.codecool.codecanvas.hu/projects/MTP/issues";
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@original-title='Main Testing Project']")));
+        WebElement mainTestingProject = driver.findElement(By.xpath("//*[@original-title='Main Testing Project']"));
+        mainTestingProject.click();
+        Assert.assertEquals(expectedURL, driver.getCurrentUrl());
     }
 
 }
