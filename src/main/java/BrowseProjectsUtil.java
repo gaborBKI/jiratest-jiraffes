423d0a4de2ee23a84073f72079e27a27178dfcf5
gaborBKI <gabor.bki@gmail.com>
2019-06-24 15:07:45 +0200

    Browseproject test refactored, added a util to avoid code dupliaction and to make it as general as possible.

---
+++ src/main/java/BrowseProjectsUtil.java
@@ -0,0 +1,34 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BrowseProjectsUtil {
+
+    private WebDriver driver;
+    private static WebDriverWait wait;
+
+    public BrowseProjectsUtil(WebDriver driver) {
+        this.driver = driver;
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    public void navigateToAllProjects(){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"header\"]/nav")));
+        WebElement projectsDropdown = driver.findElement(By.id("browse_link"));
+        projectsDropdown.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("project_view_all_link_lnk")));
+        WebElement viewAllButton = driver.findElement(By.id("project_view_all_link_lnk"));
+        viewAllButton.click();
+
+    }
+
+    public void checkForProjectInList(String projectTitle){
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@original-title=" + "'" + projectTitle + "'" + "]")));
+        WebElement mainTestingProject = driver.findElement(By.xpath("//*[@original-title='Main Testing Project']"));
+        mainTestingProject.click();
+    }
+
+
+}
--- src/test/java/BrowseProjectsTest.java
+++ src/test/java/BrowseProjectsTest.java
@@ -1,21 +1,21 @@
 import org.junit.Assert;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BrowseProjectsTest {
 
     private static WebDriver driver;
     private static Util util;
+    private static BrowseProjectsUtil browseProjectsUtil;
 
-    @BeforeAll
-    public static void setUp() {
+    @BeforeEach
+    public void setUp() {
         switch (System.getenv("driverType")) {
             case "Chrome":
                 driver = new ChromeDriver();
@@ -24,6 +24,7 @@ public class BrowseProjectsTest {
                 driver = new FirefoxDriver();
                 break;
         }
+        browseProjectsUtil = new BrowseProjectsUtil(driver);
         util = new Util(driver);
         util.navigateToPage();
         util.loginToSite(System.getenv("jiraUser"), System.getenv(("jiraPass")));
@@ -31,13 +32,7 @@ public class BrowseProjectsTest {
 
     @Test
     public void projectListAppears() {
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"header\"]/nav")));
-        WebElement projectsDropdown = driver.findElement(By.id("browse_link"));
-        projectsDropdown.click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("project_view_all_link_lnk")));
-        WebElement viewAllButton = driver.findElement(By.id("project_view_all_link_lnk"));
-        viewAllButton.click();
+        browseProjectsUtil.navigateToAllProjects();
         WebElement projectList = driver.findElement(By.id("browse-projects-page"));
         Assert.assertNotNull(projectList);
     }
@@ -45,11 +40,14 @@ public class BrowseProjectsTest {
     @Test
     public void projectPageValid(){
         String expectedURL = "https://jira.codecool.codecanvas.hu/projects/MTP/issues";
-        WebDriverWait wait = new WebDriverWait(driver, 10);
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@original-title='Main Testing Project']")));
-        WebElement mainTestingProject = driver.findElement(By.xpath("//*[@original-title='Main Testing Project']"));
-        mainTestingProject.click();
+        browseProjectsUtil.navigateToAllProjects();
+        browseProjectsUtil.checkForProjectInList("Main Testing Project");
         Assert.assertEquals(expectedURL, driver.getCurrentUrl());
+    }
+
+    @AfterEach
+    public void tearDown(){
+        driver.close();
     }
 
 }
