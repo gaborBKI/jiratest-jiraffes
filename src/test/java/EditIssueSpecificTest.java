0884301db7e559edb7b07c3e96fe21faae8d438d
Roland Gonczi <gonczi.roland@gmail.com>
2019-06-25 10:39:22 +0200

    deleteRequiredFieldsTest ready

--- src/test/java/EditIssueSpecificTest.java
+++ src/test/java/EditIssueSpecificTest.java
@@ -30,4 +30,30 @@ public class EditIssueSpecificTest {
         util.loginToSite(System.getenv("jiraUser"), System.getenv(("jiraPass")));
     }
 
+    @Test
+    public void deleteRequiredFieldsTest() {
+        WebDriverWait waitForMainPage = new WebDriverWait(driver, 10);
+        WebElement elementMainPage = waitForMainPage.until(
+                ExpectedConditions.visibilityOfElementLocated(By.id("header-details-user-fullname")));
+        util.navigateToPage("https://jira.codecool.codecanvas.hu/browse/SAND-40");
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+        WebElement element = wait.until(
+                ExpectedConditions.visibilityOfElementLocated(By.id("edit-issue")));
+        WebElement editButton = driver.findElement(By.id("edit-issue"));
+        editButton.click();
+        WebElement editIssueDialogWait = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("edit-issue-dialog")));
+        WebElement summary = driver.findElement(By.id("summary"));
+        summary.click();
+        summary.clear();
+        WebElement editIssueSubmit = driver.findElement(By.id("edit-issue-submit"));
+        editIssueSubmit.click();
+        WebElement waitForError = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("error")));
+        WebElement error = driver.findElement(By.className("error"));
+        WebElement issueType = driver.findElement(By.id("issuetype-field"));
+        issueType.click();
+        issueType.clear();
+        Assert.assertNotNull(error);
+        Assert.assertNotNull(issueType);
+    }
+
 }
