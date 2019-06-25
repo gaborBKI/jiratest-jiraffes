20682165bacb0cdd20a744ffd2dd013656418f95
Erika Toth <erikatoth.0831@gmail.com>
2019-06-25 10:50:31 +0200

    Test if Edit page opens

--- src/test/java/EditIssueGeneralTest.java
+++ src/test/java/EditIssueGeneralTest.java
@@ -1,19 +1,22 @@
+import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class EditIssueGeneralTest {
 
     private static WebDriver driver;
     private static Util util;
 
-    @BeforeEach
-    public void setUp() {
-
-        switch (System.getenv("driverType")) {
+    @BeforeAll
+    public static void setUp(){
+        switch (System.getenv("driverType")){
             case "Chrome":
                 driver = new ChromeDriver();
                 break;
@@ -21,17 +24,22 @@ public class EditIssueGeneralTest {
                 driver = new FirefoxDriver();
                 break;
         }
-
         util = new Util(driver);
         util.navigateToPage();
-
+        util.loginToSite(System.getenv("jiraUser"), System.getenv(("jiraPass")));
     }
 
     @Test
     public void editPageOpensTest() {
-
+        driver.navigate().to("https://jira.codecool.codecanvas.hu/browse/SAND-40");
+        driver.manage().window().maximize();
+        System.out.println("page opened");
+        driver.findElement(By.linkText("Edit")).click();
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+        Assert.assertNotNull(driver.findElement(By.id("edit-issue-dialog")));
     }
 
+    /*
     @Test
     public void editToucanGeneralTest() {
 
@@ -46,6 +54,7 @@ public class EditIssueGeneralTest {
     public void editJetiGeneralTest() {
 
     }
+     */
 
     @AfterEach
     public void tearDown() {
