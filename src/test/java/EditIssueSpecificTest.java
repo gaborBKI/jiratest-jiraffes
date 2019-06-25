37a570a33478caf252d842482a906000960d31f3
Roland Gonczi <gonczi.roland@gmail.com>
2019-06-25 09:35:04 +0200

    EditIssueSpecificTest class created

---
+++ src/test/java/EditIssueSpecificTest.java
@@ -0,0 +1,33 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class EditIssueSpecificTest {
+
+    private static WebDriver driver;
+    private static Util util;
+
+    @BeforeAll
+    public static void setUp(){
+        switch (System.getenv("driverType")){
+            case "Chrome":
+                driver = new ChromeDriver();
+                break;
+            case "Firefox":
+                driver = new FirefoxDriver();
+                break;
+        }
+        util = new Util(driver);
+        util.navigateToPage();
+        util.loginToSite(System.getenv("jiraUser"), System.getenv(("jiraPass")));
+    }
+
+}
